87a052e7242b57a579b7caee846f7421822f6cc9
Shivam <devshivam018@gmail.com>
2025-12-08 00:20:39 +0530

    latest changes for form

--- src/components/ui/mantineForm.tsx
+++ src/components/ui/mantineForm.tsx
@@ -1,35 +1,50 @@
 import {
-  Box,
-  Button,
-  Checkbox,
-  Group,
-  Radio,
-  TextInput,
-  Title
+    Box,
+    Button,
+    Checkbox,
+    Group,
+    Radio,
+    TextInput,
+    Title
 } from "@mantine/core";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function EnquiryForm() {
     const [loading, setLoading] = useState(false);
-    const [isSubmitted,setIsSubmitted] = useState(false);
-    const handleSubmit = async (e:any) => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
         setLoading(true);
 
         const form = e.target;
         const formData = new FormData(form);
 
-        await fetch(
-            "https://docs.google.com/forms/d/e/1FAIpQLSfA7NUWMy6-eaZXjz6u-fD7kUGQDyNrWCiLvEzTPsauNsL5hQ/formResponse",
-            {
-                method: "POST",
-                mode: "no-cors",
-                body: formData,
-            }
-        );
+        const data = {
+            name: formData.get("entry.207062940"),
+            email: formData.get("entry.1389094561"),
+            phone: formData.get("entry.1007918197"),
+            company: formData.get("entry.1010406001"),
+            country: formData.get("entry.402125416"),
+            products: "FYS",
+            form: formData.get("entry.566987055_sentinel"),
+        };
 
-        setLoading(false);
-        setIsSubmitted(true);
+        try {
+            const res = await fetch("https://script.google.com/macros/s/AKfycbyn3KcPH6CtlLjvoM8W6DrxQCeNFOf4s87v26zpVa7dnbIZ48FVGpAqhJ_1Ovj0A7Ex/exec", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                mode: "no-cors",
+                body: JSON.stringify(data),
+            });
+            console.log("hello");
+            console.log(res);
+            console.log(await res.text());
+            setIsSubmitted(true);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -51,127 +66,127 @@ export default function EnquiryForm() {
                 </div>
             ) : (
                 <>
-            <Title order={2} mb="lg">
-                Enquiry Form
-            </Title>
+                    <Title order={2} mb="lg">
+                        Enquiry Form
+                    </Title>
 
-            <form onSubmit={handleSubmit}>
-                {/* Full Name */}
-                <TextInput
-                    name="entry.207062940"
-                    label="Full name"
-                    placeholder="Enter your full name"
-                    required
-                    mb="md"
-                />
+                    <form onSubmit={handleSubmit}>
+                        {/* Full Name */}
+                        <TextInput
+                            name="entry.207062940"
+                            label="Full name"
+                            placeholder="Enter your full name"
+                            required
+                            mb="md"
+                        />
 
-                {/* Email */}
-                <TextInput
-                    name="entry.1389094561"
-                    type="email"
-                    label="Email"
-                    placeholder="Enter your email"
-                    required
-                    mb="md"
-                />
+                        {/* Email */}
+                        <TextInput
+                            name="entry.1389094561"
+                            type="email"
+                            label="Email"
+                            placeholder="Enter your e-mail"
+                            required
+                            mb="md"
+                        />
 
-                {/* Mobile Number */}
-                <TextInput
-                    name="entry.1007918197"
-                    label="Mobile number"
-                    placeholder="Enter mobile number"
-                    required
-                    mb="md"
-                />
+                        {/* Mobile Number */}
+                        <TextInput
+                            name="entry.1007918197"
+                            label="Mobile number"
+                            placeholder="Enter mobile number"
+                            required
+                            mb="md"
+                        />
 
-                {/* Company */}
-                <TextInput
-                    name="entry.1010406001"
-                    label="Company / Organisation"
-                    placeholder="Enter company name"
-                    required
-                    mb="md"
-                />
+                        {/* Company */}
+                        <TextInput
+                            name="entry.1010406001"
+                            label="Company / Organisation"
+                            placeholder="Enter company name"
+                            required
+                            mb="md"
+                        />
 
-                {/* Country */}
-                <TextInput
-                    name="entry.402125416"
-                    label="Country"
-                    placeholder="Enter your country"
-                    required
-                    mb="md"
-                />
-                <Checkbox.Group
-                    // name="entry.697430584_sentinel"
-                    label="Product of Interest"
-                    description="Select all applicable products"
-                    withAsterisk
-                >
-                    <Group mt="xs" mb="md">
-                      <div className="grid grid-cols-2 gap-2 ">
-                        <Checkbox
-                            value="Ashwagandha (Roots / Powder)"
-                            label="Ashwagandha (Roots / Powder)"
+                        {/* Country */}
+                        <TextInput
+                            name="entry.402125416"
+                            label="Country"
+                            placeholder="Enter your country"
+                            required
+                            mb="md"
                         />
-                        <Checkbox
-                            value="Aloe Vera (Powder / Flakes)"
-                            label="Aloe Vera (Powder / Flakes)"
-                        />
-                        <Checkbox
-                            value="Giloy (Stem powder / Cut Pieces)"
-                            label="Giloy (Stem powder / Cut Pieces)"
-                        />
-                        <Checkbox
-                            value="Neem (Leaf Powder, Seeds, Oil)"
-                            label="Neem (Leaf Powder, Seeds, Oil)"
-                        />
-                        <Checkbox
-                            value="Moringa (Leaf Powder)"
-                            label="Moringa (Leaf Powder)"
-                        />
-                        <Checkbox
-                            value="Shatavari (Dried / Powder)"
-                            label="Shatavari (Dried / Powder)"
-                        />
-                        <Checkbox
-                            value="Triphala (Dried / Powder)"
-                            label="Triphala (Dried / Powder)"
-                        />
-                        <Checkbox
-                            value="Turmeric (Dried / Powder)"
-                            label="Turmeric (Dried / Powder)"
-                        />
-                        <Checkbox
-                            value="Tulsi- Holy Basil (Shade Dried / Powder)"
-                            label="Tulsi- Holy Basil (Shade Dried / Powder)"
-                        />
-                        <Checkbox
-                            value="Brahmi (Shade Dried / Powder)"
-                            label="Brahmi (Shade Dried / Powder)"
-                        />
-                        </div>
-                    </Group>
-                </Checkbox.Group>
-
-                {/* Form */}
-                <Box mb="md">
-                    <Radio.Group name="entry.566987055_sentinel" label="Form" >
-                      <div className="grid grid-flow-col gap-2 justify-start">
-                        <Radio value="Raw" label="Raw" />
-                        <Radio
-                            value="Powder (Grinded/Processed)"
-                            label="Powder (Grinded/Processed)"
-                            />
-                            </div>
-                    </Radio.Group>
-                </Box>
-
-                <Button type="submit" loading={loading}>
-                    Submit
-                </Button>
-            </form>
+                        <Checkbox.Group
+                            // name="entry.697430584_sentinel"
+                            label="Product of Interest"
+                            description="Select all applicable products"
+                            withAsterisk
+                        >
+                            <Group mt="xs" mb="md">
+                                <div className="grid grid-cols-2 gap-2 ">
+                                    <Checkbox
+                                        value="Ashwagandha (Roots / Powder)"
+                                        label="Ashwagandha (Roots / Powder)"
+                                    />
+                                    <Checkbox
+                                        value="Aloe Vera (Powder / Flakes)"
+                                        label="Aloe Vera (Powder / Flakes)"
+                                    />
+                                    <Checkbox
+                                        value="Giloy (Stem powder / Cut Pieces)"
+                                        label="Giloy (Stem powder / Cut Pieces)"
+                                    />
+                                    <Checkbox
+                                        value="Neem (Leaf Powder, Seeds, Oil)"
+                                        label="Neem (Leaf Powder, Seeds, Oil)"
+                                    />
+                                    <Checkbox
+                                        value="Moringa (Leaf Powder)"
+                                        label="Moringa (Leaf Powder)"
+                                    />
+                                    <Checkbox
+                                        value="Shatavari (Dried / Powder)"
+                                        label="Shatavari (Dried / Powder)"
+                                    />
+                                    <Checkbox
+                                        value="Triphala (Dried / Powder)"
+                                        label="Triphala (Dried / Powder)"
+                                    />
+                                    <Checkbox
+                                        value="Turmeric (Dried / Powder)"
+                                        label="Turmeric (Dried / Powder)"
+                                    />
+                                    <Checkbox
+                                        value="Tulsi- Holy Basil (Shade Dried / Powder)"
+                                        label="Tulsi- Holy Basil (Shade Dried / Powder)"
+                                    />
+                                    <Checkbox
+                                        value="Brahmi (Shade Dried / Powder)"
+                                        label="Brahmi (Shade Dried / Powder)"
+                                    />
+                                </div>
+                            </Group>
+                        </Checkbox.Group>
+                        {/* Form */}
+                        <Box mb="md">
+                            <Radio.Group name="entry.566987055_sentinel" label="Form" >
+                                <div className="grid grid-flow-col gap-2 justify-start">
+                                    <Radio value="Raw" label="Raw" />
+                                    <Radio
+                                        value="Powder (Grinded/Processed)"
+                                        label="Powder (Grinded/Processed)"
+                                    />
+                                </div>
+                            </Radio.Group>
+                        </Box>
+                        <Button type="submit" loading={loading}>
+                            Submit
+                        </Button>
+                    </form>
                 </>
             )}
         </div>
     );
 }
+
+// https://script.google.com/macros/s/AKfycbxReJpy--fh7GjsfY5v95gjFlkjMarcRIvLpCmTNW-kBLRLOAFRMlsz59AydkxVvpsJ/exec
